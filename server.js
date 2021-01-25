e45a04bede1e62b7cd96a8fa2fecc75ebe1b7b7b
Bramus <bramus@bram.us>
2021-01-25 15:57:09 +0100

    🔨 Enable CORS for local testing

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 /* eslint no-console: "off" */
 
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
 // Run static server
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'demo')));
 app.use('/js', express.static(path.join(__dirname, 'dist')));
 app.listen(8080);
